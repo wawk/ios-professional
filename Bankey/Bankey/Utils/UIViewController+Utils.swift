16df6750e01d6c3b8c279b90406dd63a31e0e883
Steven Moody <white_knight@prodigy.net>
2023-06-12 09:12:52 -0400

    Replaced deprecated method for UINavigationBar

--- Bankey/Bankey/Utils/UIViewController+Utils.swift
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -8,14 +8,25 @@
 import UIKit
 
 extension UIViewController {
+    
     func setStatusBar() {
-        let statusBarSize = UIApplication.shared.statusBarFrame.size
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusBarView = UIView(frame: frame)
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground()
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
         
-        statusBarView.backgroundColor = appColor
-        view.addSubview(statusBarView)
     }
+    
+    
+    // Deprecated ios13
+//    func setStatusBar() {
+//        let statusBarSize = UIApplication.shared.statusBarFrame.size
+//        let frame = CGRect(origin: .zero, size: statusBarSize)
+//        let statusBarView = UIView(frame: frame)
+//
+//        statusBarView.backgroundColor = appColor
+//        view.addSubview(statusBarView)
+//    }
     func setTabBarImage(imageName: String, title: String){
         let configuration = UIImage.SymbolConfiguration(scale: .large)
         let image = UIImage(systemName: imageName, withConfiguration: configuration)
